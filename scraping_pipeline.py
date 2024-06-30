632e0501834776692568fd6dd4aa774a89de6fbf
jExbrayat <jules.exbrayat@gmail.com>
2024-06-30 23:57:48 +0200

    handle errors and logs

--- scraping_pipeline.py
+++ scraping_pipeline.py
@@ -1,14 +1,23 @@
 from get_statics import scrap_write_commune_results
 from utils import get_links_from_csv
+import json
 
 communes_links = get_links_from_csv("communes_links.csv")
 
 # TODO: remove debugging
 communes_links = communes_links[:10]
 
+logs = {}  # Init logs dictionary
 for id, link in communes_links:
-    scrap_write_commune_results(
-        commune_link=link,
-        result_table_path=f"scraped_data/results_tables/{id}.csv",
-        stats_table_path=f"scraped_data/stats_tables/{id}.csv",
-    )
+    try:
+        scrap_write_commune_results(
+            commune_link=link,
+            result_table_path=f"scraped_data/results_tables/{id}.csv",
+            stats_table_path=f"scraped_data/stats_tables/{id}.csv",
+        )
+        logs[id] = "Data scraped successfully"
+    except:
+        logs[id] = "Scraping failed"
+
+with open("logs.json", "w") as logs_file:
+    json.dump(logs, logs_file)
