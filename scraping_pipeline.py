9167118e5f5ab25cdfa7d7d907033df175528693
jExbrayat <jules.exbrayat@gmail.com>
2024-07-01 15:01:33 +0200

    define function

--- scraping_pipeline.py
+++ scraping_pipeline.py
@@ -2,21 +2,24 @@ from get_statics import scrap_write_commune_results
 from utils import get_links_from_csv
 import json
 
-communes_links = get_links_from_csv("communes_links.csv")
 
-logs = {}  # Init logs dictionary
-for id, link in communes_links:
-    try:
-        scrap_write_commune_results(
-            commune_link=link,
-            result_table_path=f"scraped_data/results_tables/{id}.csv",
-            stats_table_path=f"scraped_data/stats_tables/{id}.csv",
-        )
-        print(f"{id} scraped successfully")
-        logs[id] = "Data scraped successfully"
-    except:
-        print(f"{id} scraping failed")
-        logs[id] = "Scraping failed"
+def scrap_and_write(
+    communes_links: list[list], logs_path: str, save_folder: str
+) -> None:
 
-with open("logs.json", "w") as logs_file:
-    json.dump(logs, logs_file)
+    logs = {}  # Init logs dictionary
+    for id, link in communes_links:
+        try:
+            scrap_write_commune_results(
+                commune_link=link,
+                result_table_path=f"{save_folder}/results_tables/{id}.csv",
+                stats_table_path=f"{save_folder}/stats_tables/{id}.csv",
+            )
+            print(f"{id} scraped successfully")
+            logs[id] = "Data scraped successfully"
+        except:
+            print(f"{id} scraping failed")
+            logs[id] = "Scraping failed"
+
+    with open(logs_path, "w") as logs_file:
+        json.dump(logs, logs_file)
